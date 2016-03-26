bc43e6804b5e6707eea2b2424ad76be01b5a28da
revitalk <revital.kaisar@gmail.com>
2016-03-26 17:02:43 -0400

    Footer with static copyright statement and a link to the Contact Us page
    For the contact link to work you must name the page: Contact Us

--- footer.php
+++ footer.php
@@ -15,7 +15,8 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			&copy; Association for the Promotion of New Music
+			&copy; Association for the Promotion of New Music | 
+			<a href="<?php echo get_permalink( get_page_by_title( 'Contact Us' ) ); ?>">Contact Us</a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
