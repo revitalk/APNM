22b43b8e0f36d79f6cac5ef9b9a0bff2c9d8eb04
revitalk <revital.kaisar@gmail.com>
2016-03-26 12:40:32 -0400

    Added files via upload

--- footer.php
+++ footer.php
@@ -15,9 +15,10 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'apnm' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'apnm' ), 'WordPress' ); ?></a>
+			<!-- <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'apnm' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'apnm' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'apnm' ), 'apnm', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'apnm' ), 'apnm', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?> -->
+			&copy; Association for the Promotion of New Music
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
